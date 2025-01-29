a844e05d216c0eb4466913971c4d4f5aa06bda08
Jawahar <jawahar.as@shinelogics.com>
2025-01-30 00:56:00 +0530

    update

--- src/pages/Career/index.tsx
+++ src/pages/Career/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import {
   Heading,
@@ -16,7 +16,7 @@ import {
 import { jobData } from "./jobdata"; // Importing job openings dynamically
 
 const Career = () => {
-  const [selectedJob, setSelectedJob] = useState(null);
+  // const [selectedJob, setSelectedJob] = useState(null);
 
   return (
     <>
